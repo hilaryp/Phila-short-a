dbc15fbca512121c2e795e9086632362ea8378ed
Hilary Prichard <hilary.e.p@gmail.com>
2014-09-25 18:32:39 -0400

    added token mins before write out

--- PNC-processing.R
+++ PNC-processing.R
@@ -14,15 +14,13 @@ info <- read.csv("~/Desktop/Higher_Ed/Data/SpkInfo.csv")
 # Process original coding
 olda <- read.csv("~/Desktop/Higher_Ed/Data/PNC-2014-09-16.csv")
 olda2 <- merge(olda, info, all.x=TRUE, all.y=FALSE)
-olda3 <- droplevels(subset(olda2, 
-                           !Ethnicity %in% c("", "a", "a/o", "a/w", "h", "o",
-                                             "s", "s/p", "u", "b", "b/w")
-                           & Age >= 18))
+olda3 <- droplevels(subset(olda2, Ethnicity == 'w' & Age >= 18))
+olda3$Speaker <- NULL
 
 # Process new coding
-newa <- read.csv("~/Desktop/Higher_Ed/Data/PNC-2014-09-15-newa.csv")
-newa2 <- merge(newa, info, all.x=TRUE, all.y=FALSE)
-newa3 <- droplevels(subset(newa2, Ethnicity == "w" & Age >= 18))
+#newa <- read.csv("~/Desktop/Higher_Ed/Data/PNC-2014-09-15-newa.csv")
+#newa2 <- merge(newa, info, all.x=TRUE, all.y=FALSE)
+#newa3 <- droplevels(subset(newa2, Ethnicity == 'w' & Age >= 18))
 
 
 # monosyllablic function words from Selkirk 1984:352-353
@@ -41,23 +39,41 @@ STOPWORDS <- c('A', 'ALL', 'AM', 'AN', 'AND', "AN'", 'ARE', "AREN'T",
                'WILL', 'WITH', 'WOULD', 'YEAH', 'YOU', 'YOUR', 'BASIL')
 
 olda4 <- droplevels(subset(olda3, !Word %in% STOPWORDS))
-newa4 <- droplevels(subset(newa3, !Word %in% STOPWORDS))
+#newa4 <- droplevels(subset(newa3, !Word %in% STOPWORDS))
 
 olda5 <- ddply(olda4, .(Subject), transform, Z1=scale(F1), Z2=scale(F2), 
                DOB=Year-Age)
-newa5 <- ddply(newa4, .(Subject), transform, Z1=scale(F1), Z2=scale(F2), 
-               DOB=Year-Age)
+#newa5 <- ddply(newa4, .(Subject), transform, Z1=scale(F1), Z2=scale(F2), 
+#               DOB=Year-Age)
 
-olda5$M1=mel(olda5$F1)
-olda5$M2=mel(olda5$F2)
-newa5$M1=mel(newa5$F1)
-newa5$M2=mel(newa5$F2)
+olda6 <- transform(olda5, M1=mel(F1), M2=mel(F2))
+#newa6 <- transform(newa5, M1=mel(F1), M2=mel(F2))
 
-olda6 <- ddply(olda5, .(Subject), transform, MZ1=scale(M1), MZ2=scale(M2))
-newa6 <- ddply(newa5, .(Subject), transform, MZ1=scale(M1), MZ2=scale(M2))
+olda7 <- ddply(olda6, .(Subject), transform, MZ1=scale(M1), MZ2=scale(M2))
+#newa6 <- ddply(newa5, .(Subject), transform, MZ1=scale(M1), MZ2=scale(M2))
 
-oldaf <- droplevels(subset(olda6, VClass %in% c('ae','aeh','aeBR')))
-newaf <- droplevels(subset(newa6, VClass %in% c('ae','aeh','aeBR')))
+olda8 <- droplevels(subset(olda7, VClass %in% c('ae','aeh','aeBR')))
+#newa8 <- droplevels(subset(newa6, VClass %in% c('ae','aeh','aeBR')))
+
+# Remove words with less than 5 tokens
+counts <- as.data.frame(table(olda8$Word))
+olda9 <- merge(olda8, counts, by.x="Word", by.y="Var1")
+olda10 <- droplevels(subset(olda9, Freq >=5))
+
+# Remove speakers with less than 5 tokens in each category
+for (speaker in levels(olda10$Subject)) {
+  data <- subset(olda10, Subject==speaker)
+  ae.d <- data[data$VClass=="ae", ]
+  aeh.d <- data[data$VClass=="aeh", ]
+  if(nrow(ae.d) < 5 | nrow(aeh.d) < 5) print(speaker)
+}
+
+oldaf <- droplevels(subset(olda10, !Subject %in% c("PH10-2-10", "PH10-2-11",
+                    "PH74-00-6", "PH78-5-3", "PH79-3-8", "PH79-4-5",
+                    "PH80-2-8", "PH84-1-3", "PH84-2-10", "PH85-3-8",
+                    "PH86-3-3", "PH90-2-2", "PH91-2-19", "PH91-2-21",
+                    "PH91-2-3", "PH91-2-8", "PH94-1-1")
+                    & Stress == 1))
 
 write.csv(oldaf, "shorta-old.csv", row.names=FALSE, quote=FALSE)
 write.csv(newaf, "shorta-new.csv", row.names=FALSE, quote=FALSE)
